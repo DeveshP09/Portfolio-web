88714d299cb00615ed91e14e22e48465e27a859d
devesh <deveshpatil9000@gmail.com>
2025-01-29 22:38:05 +0530

    "started"

---
+++ Portfolio website/src/Components/Layout.jsx
@@ -0,0 +1,21 @@
+
+
+const Layout = () => {
+    return(
+        <>
+        <div className="sidebar-section">
+            <div className="sidebar">
+                <div className="sidebar-toggle-icons">
+                    icon
+                </div>
+            </div>
+            <div className="container">
+                
+            </div>
+        </div>
+        </>
+    )
+
+};
+
+export default Layout;
