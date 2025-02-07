94e64c94fedbdb365256b35959623584f438e524
devesh <deveshpatil9000@gmail.com>
2025-02-08 00:58:05 +0530

    "updated"

--- Portfolio website/src/App.jsx
+++ Portfolio website/src/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./Components/Layout";
-
+import About from "./Pages/About/About";
 
 function App() {
- 
   return (
     <>
-      <Layout/>
+      <Layout />
+      <div className="container">
+        <About></About>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ Portfolio website/src/Pages/About/About.jsx
@@ -0,0 +1,43 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        <div className="row">
+          <div className="col-md-5  about-image">
+            <img src="../../public/devesh.jpg" alt="image" />
+          </div>
+          <div className="col-md-6 about-content">
+            <h1>About Me</h1>
+            <p>
+              <ul>
+                <li>
+                  I am Devesh Patil, graduated from Mumbai University with
+                  degree in Information Technology
+                </li>
+                <li>
+                  Adaptive learner, Passionate about staying up-to-date with the
+                  latest technologies
+                </li>
+                <li>
+                  Having expertise in Java, Python, Javascript, React.js and SQL
+                </li>
+                <li>
+                  Commitment to lifelong learning and to adapt new technologies
+                  to tackle the next big challenge.
+                </li>
+                <li>
+                  Regularly setting personal objectives that complement the
+                  company's technological advancements
+                </li>
+              </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
