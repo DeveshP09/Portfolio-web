b32b569edbfb0a4dd25f473b7f655fb034ac255c
devesh <deveshpatil9000@gmail.com>
2025-02-07 13:28:43 +0530

    "updated"

--- Portfolio website/src/Components/Layout.jsx
+++ Portfolio website/src/Components/Layout.jsx
@@ -16,17 +16,17 @@ const Layout = () => {
     <>
       <div className="sidebar-section">
         <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
-          {/*As we pass the two class for the true condition, so all the properties of clas one we be applied frist and then reamining from class two*/}
+          {/*As we pass the two class for the true condition, so all the properties of class one we be applied frist and then reamining from class two*/}
           <div className="sidebar-toggle-icons">
-            <Menus toggle={toggle}/>
             <p onClick={handleToggle}>
               {toggle ? (
-                <AiOutlineDoubleLeft size={30} />
+                <AiOutlineDoubleLeft size={30} /> 
               ) : (
                 <AiOutlineDoubleRight size={30} />
               )}
             </p>
           </div>
+          <Menus toggle={toggle} />
         </div>
         <div className="container">
           <Home></Home>
--- Portfolio website/src/Components/Menus.jsx
+++ Portfolio website/src/Components/Menus.jsx
@@ -1,4 +1,8 @@
 import "./menus.css";
+import { IoHome, IoInformationCircleSharp } from "react-icons/io5";
+import { FaGraduationCap, FaLaptopCode } from "react-icons/fa6";
+import { MdEngineering, MdWebStories } from "react-icons/md";
+import { IoMdContact } from "react-icons/io";
 
 const Menus = ({ toggle }) => {
   return (
@@ -8,9 +12,91 @@ const Menus = ({ toggle }) => {
           <div className="navbar-profile-pic">
             <img src="../public/profile2.png" alt="profile pic" />
           </div>
+          <div className="nav-items">
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoHome />
+                Home
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoInformationCircleSharp />
+                About
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <FaGraduationCap />
+                Education
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <MdEngineering />
+                Work Experience
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <FaLaptopCode />
+                Skills
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <MdWebStories />
+                Projects
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoMdContact />
+                Contact
+              </div>
+            </div>
+          </div>
         </>
-      ) : (
-        <h1>icons</h1>
+      ) : ( //toggle   button functionality
+        <>
+          <div className="nav-items">
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoHome />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoInformationCircleSharp />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <FaGraduationCap />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <MdEngineering />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <FaLaptopCode />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <MdWebStories />
+              </div>
+            </div>
+            <div className="nav-item">
+              <div className="nav-link">
+                <IoMdContact />
+              </div>
+            </div>
+          </div>
+        </>
       )}
     </>
   );
